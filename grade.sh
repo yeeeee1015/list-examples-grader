86fb7ee954b7658cedc284bbcd594887101af70b
yeeeee1015 <eric.ye04@yahoo.com>
2023-02-15 16:50:01 -0800

    Made the implementation

--- grade.sh
+++ grade.sh
@@ -1,5 +1,34 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+echo 'Found Thing!'
+else 
+echo 'Need ListExamples.java'
+exit 1
+fi
+
+cp ../TestListExamples.java .
+echo 'Moved TestListExamples.java'
+cp -r ../lib .
+echo 'Copied junit libs'
+
+javac -cp $CPATH *.java > ../compileOutput.txt
+echo 'Done compiling'
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > ../output.txt
+echo 'Code has run'
+
+if [[ $(grep -h "OK" ../output.txt) != "" ]]
+then
+    echo 'There are no errors! Congrats!'
+elif [[ $(grep -h "testMergeRightEnd" ../output.txt) != "" ]]
+then
+    echo 'You are failing the test "testMergeRightEnd"'
+fi
